814e5e740c38b27c9d406bb08021cbd19688620e
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-05-18 10:49:37 +0100

    Sync

---
+++ src/bin/show_posts.rs
@@ -0,0 +1,22 @@
+use diesel::prelude::*;
+use diesel_demo::*;
+
+use self::models::*;
+
+fn main() {
+    use self::schema::posts::dsl::*;
+
+    let connection = &mut establish_connection();
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("{}\n", "=".repeat(post.title.len()));
+        println!("{}", post.body);
+    }
+}
